2e458f03cf91ebc710bbb45fdc8caf6f71301e48
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-04-25 23:17:42 +0530

    Update app.ts

--- typeorm-master/sample22-closure-table/app.ts
+++ typeorm-master/sample22-closure-table/app.ts
@@ -6,7 +6,7 @@ const options: ConnectionOptions = {
     type: "mysql", 
     host: "localhost",   
     port: 3306,    
-    username: "root",     
+    username: "root",      
     password: "admin",  
     database: "test", 
     logging: ["query", "error"], 
